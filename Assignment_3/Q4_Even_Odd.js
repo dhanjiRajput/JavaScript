8282ff20134a7f6fbdd5d75885662881e4399277
dhanjiRajput <kidechadhanji@gmail.com>
2024-04-18 23:28:23 +0530

    Assignment

---
+++ Assignment_3/Q4_Even_Odd.js
@@ -0,0 +1,5 @@
+let n1=1;
+
+(n1%2==0)
+        ? console.log("The Given Number Is Even...")
+        : console.log("The Given Number Is Odd....");
---
+++ Assignment_3/Q9_Apply_Brake.js
@@ -0,0 +1,8 @@
+let distance=100;
+let time=2;
+
+let speed=distance/time;
+
+(speed>40)
+        ? console.log("Apply Brake")
+        : console.log("Keep Going...");
