819535f7adac625621e4025edbf110370533ea6b
dhanjiRajput <kidechadhanji@gmail.com>
2024-05-23 11:43:36 +0530

    Updated

--- Counter/index.js
+++ Counter/index.js
@@ -29,33 +29,47 @@ switch (day) {
 //Find Time
 
 const time = () => {
+    let h;
     setInterval(() => {
         let time_zone1=new Date();
 
         let hours = time_zone1.getHours();
         let minute = time_zone1.getMinutes();
         let second = time_zone1.getSeconds();
-        document.getElementById("time").append(`${hours} : ${minute} : ${second}`);
-    }, 1000);
 
-    document.getElementById("time").innerHTML = "";
+        minute=zero(minute);
+        second=zero(second);
+
+        if(hours<13){
+            h="am";
+        }
+        else{
+            h="pm";
+        }
+        document.getElementById("time").innerHTML=`${hours} : ${minute} : ${second}  ${h}`;
+    }, 1000);
 }
 
-// time();
+const zero=(i)=>{
+    if(i<10){
+        i="0"+i;
+    }
+    return i;
+}
+
+time();
 
 //Find Date
-
+let month2;
 let date=time_zone.getDate();
 let month=time_zone.getMonth();
 let year=time_zone.getFullYear();
-let month2;
 
 let month1=["JANUARY","FEBRUARY","MARCH","APRIL","MAY","JUNE","JULY","AUGUST","SEPTEMBER","OCTOBER","NOVEMBER","DECEMBER"];
 
 for(let i=0;i<month1.length;i++){
     if(month==i){
-        month2=arr[i];
+        month2=month1[i];
     }
 }
-
-// document.getElementById("date").append(`${date}th : ${month} : ${year}`);
+document.getElementById("date").append(`${date} - ${month2} - ${year}`);
