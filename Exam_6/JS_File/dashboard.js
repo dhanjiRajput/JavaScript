452f7c901eac4ffdc25cd004ea8b6287c2351027
dhanjiRajput <kidechadhanji@gmail.com>
2024-06-22 17:53:11 +0530

    updated Exam

--- Exam_6/JS_File/dashboard.js
+++ Exam_6/JS_File/dashboard.js
@@ -3,10 +3,20 @@ let books = JSON.parse(localStorage.getItem("books")) || [];
 let mybook = JSON.parse(localStorage.getItem("mybook")) || [];
 let bookmark = JSON.parse(localStorage.getItem("bookmark")) || [];
 
+let count=books.length;
+document.getElementById("book_count").innerHTML = `Total Available Books : ${count}`;
+
+const delete_row=(index)=>{
+    books.splice(index,1);
+    mapper(books);
+    localStorage.setItem("books",JSON.stringify(books));
+    location.reload();
+}
+
 const mapper = (data) => {
     document.getElementById("list").innerHTML = "Loading Data.....";
 
-    data.map((item) => {
+    data.map((item,index) => {
 
         let td1 = document.createElement("td");
         td1.innerHTML = item.book_name;
@@ -27,23 +37,26 @@ const mapper = (data) => {
         td6.innerHTML = item.price;
 
         let btn1 = document.createElement("button");
-        btn1.innerHTML = "Buy Now";
+        btn1.innerHTML = "Buy";
         btn1.setAttribute("id", "btn");
         let td7 = document.createElement("td");
         td7.append(btn1);
         btn1.addEventListener("click", () => {
             mybook.push(item);
             localStorage.setItem("mybook", JSON.stringify(mybook));
+            delete_row(index);
+            
         });
 
         let btn2 = document.createElement("button");
-        btn2.innerHTML = "BookMark";
+        btn2.innerHTML = "Add";
         btn2.setAttribute("id", "btn");
         let td8 = document.createElement("td");
         td8.append(btn2);
         btn2.addEventListener("click", () => {
             bookmark.push(item);
             localStorage.setItem("bookmark", JSON.stringify(bookmark));
+            delete_row(index);
         });
 
         let tr1 = document.createElement("tr");
@@ -54,3 +67,18 @@ const mapper = (data) => {
 }
 
 mapper(books);
+
+//Filter Search
+const searchvalue=(value)=>{
+    let temp=books.filter((item)=>item.category==value);
+    let n=temp.length;
+    document.getElementById("book_count").innerHTML = `Total Available Books : ${n}`;
+    return mapper(temp);
+}
+const handlesearch=(e)=>{
+    e.preventDefault();
+
+    let value=document.getElementById("search_input").value
+    searchvalue(value);
+}
+document.getElementById("search_category").addEventListener("submit",handlesearch);
--- Exam_6/index.js
+++ Exam_6/index.js
@@ -1,20 +1,28 @@
 
 //Store Books Data To Local Storage
-let push_data=JSON.parse(localStorage.getItem("books"))||[];
-const HandleData=(e)=>{
+let push_data = JSON.parse(localStorage.getItem("books")) || [];
+const HandleData = (e) => {
     e.preventDefault();
 
-    let book_data={
-        book_name:document.getElementById("book_name").value,
-        book_author:document.getElementById("author_name").value,
-        description:document.getElementById("description").value,
-        date:document.getElementById("date").value,
-        category:document.getElementById("category").value,
-        price:document.getElementById("price").value,
+    let book_data = {
+        book_name: document.getElementById("book_name").value,
+        book_author: document.getElementById("author_name").value,
+        description: document.getElementById("description").value,
+        date: document.getElementById("date").value,
+        category: document.getElementById("category").value,
+        price: document.getElementById("price").value,
     }
-    
-    push_data.push(book_data);
-    localStorage.setItem("books",JSON.stringify(push_data));
-    alert("Books Added Successfully....");
+
+    let pass = "dk";
+    let p = prompt("Enter Admin Code to Add Books...");
+
+    if (pass == p) {
+        push_data.push(book_data);
+        localStorage.setItem("books", JSON.stringify(push_data));
+        alert("Books Added Successfully....");
+    } else {
+        alert("You are Not Valid Person To Add Books....");
+    }
+
 }
-document.getElementById("book_data").addEventListener("submit",HandleData);
+document.getElementById("book_data").addEventListener("submit", HandleData);
