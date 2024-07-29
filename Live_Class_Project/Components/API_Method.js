54a081e0a35ed3827e12708810758f474d0f0b6a
dhanjiRajput <kidechadhanji@gmail.com>
2024-07-29 22:49:52 +0530

    Updated

--- Live_Class_Project/Components/API_Method.js
+++ Live_Class_Project/Components/API_Method.js
@@ -1,18 +1,18 @@
 export const User_API={
     get_email:async (email)=>{
-        let req=await fetch(`http://localhost:3000/users?email=${email}`);
+        let req=await fetch(`https://db-qpsz.onrender.com/users?email=${email}`);
         let res=await req.json();
         return res;
     },
 
     get:async ()=>{
-        let req=await fetch(`http://localhost:3000/users`);
+        let req=await fetch(`https://db-qpsz.onrender.com/users`);
         let res=await req.json();
         return res;
     },
 
     post:async (data)=>{
-        let req=await fetch(`http://localhost:3000/users`,{
+        let req=await fetch(`https://db-qpsz.onrender.com/users`,{
             method:"POST",
             headers:{"Content-Type":"application/json"},
             body:JSON.stringify(data)
@@ -22,13 +22,13 @@ export const User_API={
 
 export const products_API={
     get:async()=>{
-        let req=await fetch(`http://localhost:3000/products`)
+        let req=await fetch(`https://db-qpsz.onrender.com/products`)
         let res=await req.json();
         return res;
     },
 
     post:async(data)=>{
-        let req=await fetch(`http://localhost:3000/products`,{
+        let req=await fetch(`https://db-qpsz.onrender.com/products`,{
             method:"POST",
             headers:{"Content-Type":"application/json"},
             body:JSON.stringify(data)
@@ -38,13 +38,13 @@ export const products_API={
 
 export const cart_API={
     get:async()=>{
-        let req=await fetch(`http://localhost:3000/cart`)
+        let req=await fetch(`https://db-qpsz.onrender.com/cart`)
         let res=await req.json();
         return res;
     },
 
     post:async(data)=>{
-        let req=await fetch(`http://localhost:3000/cart`,{
+        let req=await fetch(`https://db-qpsz.onrender.com/cart`,{
             method:"POST",
             headers:{"Content-Type":"application/json"},
             body:JSON.stringify(data)
@@ -52,7 +52,7 @@ export const cart_API={
     },
 
     patch:async (data,id)=>{
-        let req=await fetch(`http://localhost:3000/cart/${id}`,{
+        let req=await fetch(`https://db-qpsz.onrender.com/cart/${id}`,{
             method:"PATCH",
             headers:{"Content-Type":"application/json"},
             body:JSON.stringify(data)
@@ -60,7 +60,7 @@ export const cart_API={
     },
 
     delete:async (id)=>{
-        let req=await fetch(`http://localhost:3000/cart/${id}`,{
+        let req=await fetch(`https://db-qpsz.onrender.com/cart/${id}`,{
             method:"DELETE",
         })
     },
